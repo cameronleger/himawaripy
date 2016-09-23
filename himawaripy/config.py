f25fab5dae0ffe5d707593a60115f7b550dce83d
Cameron Leger <contact@cameronleger.com>
2016-09-23 11:47:26 -0400

    Added support for using ImageMagick to overlay an image (i.e. a galaxy) over the Himawari output before setting it as the background

--- himawaripy/config.py
+++ himawaripy/config.py
@@ -1,7 +1,14 @@
 import appdirs
+from os.path import expanduser
 
 # Increases the quality and the size. Possible values: 4, 8, 16, 20
 level = 4
+
+# Define the final output size and a file (with alpha) to composite over the output
+# For example, an image of the Milky Way as seen from Earth
+# that has an alpha cutout in the shape of the Earth from Himawari
+resize_to = "3840x2160"
+overlay_path = expanduser("~/Pictures/Himawari/himawari-overlay.png")
 
 # Define a hourly offset or let the script calculate it depending on your timezone
 # If auto_offset is True, then script will calculate your hour offset automatically depending on your location.
